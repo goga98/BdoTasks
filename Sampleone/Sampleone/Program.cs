aed43d1f2c1960826242c1e9e9f9bababae2d6f1
goga chokoraia <gchokor10@gmail.com>
2021-03-31 13:22:32 +0400

    Tasks

---
+++ Sampleone/Sampleone/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampleone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 1, 4, 5, 7, 10, 3 };
+            int[] y = { 2, 3, 12, 6 };
+            SecondTask(x,y);
+            Console.ReadKey();
+        }
+
+        static void SecondTask(int[] x, int[]y)
+        {
+            int temp = 0;
+            int[] combined = x.Concat(y).ToArray();
+            for (int i = 0; i < combined.Length; i++)
+            {
+                for (int j = i + 1; j < combined.Length; j++)
+                {
+                    if (combined[i] > combined[j])
+                    {
+                        temp = combined[j];
+                        combined[j] = combined[i];
+                        combined[i] = temp;
+                    }                  
+                }
+                Console.WriteLine(combined[i]);
+            }
+
+        }
+    }
+}
